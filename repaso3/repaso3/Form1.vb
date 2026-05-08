eee489bfc2740c859e6827e744725753ce91d107
Asher-arreguin <diego.arreguin@iest.edu.mx>
2026-05-08 08:38:04 -0600

    Add files via upload

---
+++ repaso3/repaso3/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Dim contador = 0
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'contador + contador + 1 = 0 
+        contador += 1
+        Label2.Text = contador
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        contador = 0
+        Label2.Text = contador
+    End Sub
+End Class
